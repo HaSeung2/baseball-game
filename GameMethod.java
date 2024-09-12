21f97edf8a5a839b1507168f2f74b7075af825b9
하승 <dlgktmd0@naver.com>
2024-09-12 19:06:02 +0900

    주석 추가 및 Collections.shuffle를 이용하여 랜덤숫자 생성

--- GameMethod.java
+++ GameMethod.java
@@ -1,10 +1,8 @@
 package baseBallGame;
 
 import baseBallGame.exception.BadInputException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
+
+import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.regex.Pattern;
 
@@ -47,7 +45,7 @@ public class GameMethod{
         this.result.add(result);
     }
 
-    public ArrayList<Integer> getResult(){
+    public ArrayList <Integer> getResult(){
         return result;
     }
 
@@ -59,6 +57,11 @@ public class GameMethod{
     public String createRandomNumber(){
         HashSet <String> numbers = new HashSet<>();
         String finalNumber = "";
+
+        // Set 컬렉션에는 중복값이 저장되지 않는다.
+        // 그래서 while문으로 돌리면서
+        // if문으로 Set의 길이가 유저가 입력한 자릿수와 일치한다면
+        // break로 while문 탈출.
         while(true){
             numbers.add(ThreadLocalRandom.current().nextInt(1,10)+"");
             if(numbers.size() == 3){
@@ -72,33 +75,37 @@ public class GameMethod{
         return finalNumber;
     }
 
-    public String userChoiceRandomSetting(String userChoiceNumber) throws Exception{
+    // 유저가 자릿수를 입력했을 때 그 입력한 값을 받아서 그 값만큼의 자릿수를 가진 랜덤 숫자 생성하는 메서드
+    public String createChoiceRandomNumber(String userChoiceNumber) throws Exception{
         String finalNumber = "";
         int userNumber = Integer.parseInt(userChoiceNumber);
         if(Pattern.matches(numberReg, userChoiceNumber) && (userNumber <= 5 && userNumber >= 3)){
-            HashSet <String> numbers = new HashSet<>();
             setRandomDigits(userNumber);
-            while(true){
-                numbers.add(ThreadLocalRandom.current().nextInt(1,10)+"");
-                if(numbers.size() == userNumber){
-                    break;
-                }
+            // 셔플을 이용하여 랜덤 숫자 생성
+            List <String> num = new ArrayList<>();
+            for(int i = 1; i < 10; i++){
+                num.add(i+"");
             }
-            Iterator <String> iter = numbers.iterator();
-            for(int i = 0; i < numbers.size(); i++){
-                finalNumber += iter.next();
+            Collections.shuffle(num);
+            num = num.subList(0,userNumber);
+            for(String shuNum : num){
+                finalNumber += shuNum;
             }
             return finalNumber;
         }
         throw new BadInputException("3, 4, 5 중에서만 입력 했는지");
     }
 
+    // 랜덤숫자와 유저가 입력한 숫자를 비교하여
+    // 정답인지 확인하는 메서드
     public boolean filterNum(String randomNumber,String userInput) throws Exception{
         List <String> userInputList = new ArrayList<>();
         List <String> randomList = new ArrayList<>();
         int strikeCount = 0;
         int ballCount = 0;
 
+        // 유저가 입력한 숫자가 숫자로만 이루어져있는지, 또 랜덤 숫자의 자릿수와 같은지
+        // 판별 후 맞다면 각각 스트라이크의 수와 볼의 수를 count해주는 메서드 실행.
         if(Pattern.matches(numberReg, userInput) && duplicateCheck(userInput)){
             for(int i = 0; i < userInput.length(); i++){
                   userInputList.add(userInput.substring(i, i+1));
@@ -106,14 +113,21 @@ public class GameMethod{
             }
             strikeCount =  strikeCnt(randomList,userInputList);
             ballCount = ballCnt(randomList,userInputList);
+            // 스트라이크의 갯수가 자릿수보다 작다면 정답을 맞추지 못했다는 것을 의미.
             if(strikeCount < randomDigits){
+                // 스트라이크의 갯수를 저장.
                 setStrike(strikeCount);
                if(ballCount >= 0){
+                   // 볼 카운트는 이중 for문으로 돌면서 하나하나 다 검사를 하기 때문에
+                   // 볼카운트 안에는 스트라이크 카운트도 같이 들어있다.
+                   // 그래서 볼 카운트에 스트라이크 갯수를 빼면서 대입한다.
                    ballCount -= strikeCount;
+                   // 그 볼 갯수를 저장.
                    setBall(ballCount);
                }
             }
             else{
+                // 정답이라면 만들어둔 리스트의 저장 해주면서 true 리턴.
                 setResult(Integer.parseInt(userInput));
                 return true;
             }
@@ -124,14 +138,24 @@ public class GameMethod{
         return false;
     }
 
+    // 유저가 입력한 숫자의 자릿수가 중복 숫자가 없는지와 랜덤 숫자의 자릿수와 일치한지
+    // 판별하는 메서드
     public boolean duplicateCheck(String userInput) {
         HashSet <String> numbers = new HashSet<>();
+        // set에는 중복값이 들어가면 자동으로 삭제가된다.
+        // 그러므로 입력받은 값중에 중복값이 있는지 확인하기 위하여
+        // set 컬렉션에 저장을하고나서
+        // 컬렉션의 저장된 데이터들의 배열 길이가 랜덤 숫자의 자릿수와 일치하면
+        // 중복값이 없고 자릿수의  맞는 수를 입력한것이기 떄문에
+        // true리턴해준다.
+        // 아니라면 false가 리턴.
         for(int i = 0; i < userInput.length(); i++){
             numbers.add(userInput.substring(i, i+1));
         }
         return numbers.size() == randomDigits;
     }
 
+    // 스트라이크 갯수 구하는 메서드
     public int strikeCnt(List<String> randomNumber, List<String>userInputList){
         int strike = 0;
         for(int i = 0; i < randomNumber.size(); i++){
@@ -142,6 +166,7 @@ public class GameMethod{
         return strike;
     }
 
+    // 볼 갯수 구하는 메서드
     public int ballCnt(List<String> randomNumber, List<String>userInputList){
         int ball = 0;
         for(int i = 0; i < randomNumber.size(); i++){
--- GameStart.java
+++ GameStart.java
@@ -14,37 +14,55 @@ public class GameStart {
     public boolean gameStart() throws Exception {
         Scanner sc = new Scanner(System.in);
         while (true) {
+            // 이 선택지들은 맨 처음과 문제를 맞췄을 때만 보여주기 위해
+            // 이 메서드가 시작할때와 정답을 맞췄을 때에만 num을 0으로 재 할당 시켜준다.
+            // 정상적으로 입력을 잘 하여 게임이 시작되면 num을 num++;로 증가 시켜줘서
+            // 문제를 맞추기 전까지는 이 if문 안의 코드들은 실행되지 않는다.
             if (num == 0) {
+                // 자리수 설정하기를 선택하지않고
+                // 게임 시작하기로 게임을 시작할 때를 대비하여
+                // 랜덤 숫자의 자릿수를 저장하는 randomDigits에 미리 3을 담아서
                 game.setRandomDigits(3);
+                // 3자릿수 난수를 미리 한개 생성해둔다.
                 randomNumber = game.createRandomNumber();
+
                 System.out.println("환영합니다 ! 원하시는 번호를 입력해주세요");
                 System.out.print("0. 자리수 설정하기 1. 게임 시작하기  2. 게임 기록 보기  3. 종료하기 : ");
                 String choiceNumber = sc.nextLine();
+
                 switch (choiceNumber) {
+                    // 0을 입력하면 3,4,5를 입력하라는 출력 문구와 함께
+                    // 입력 받은 숫자를  createChoiceRandomNumber() 메서드를 호출하며 전달.
                     case "0":
                         System.out.print("3, 4, 5자릿수 중에 골라주세요 ~ : ");
-                        String userChoiceNumber = sc.nextLine();
-                        randomNumber = game.userChoiceRandomSetting(userChoiceNumber);
+                        String userChoiceDigits = sc.nextLine();
+                        // 3, 4, 5 중에 입력을 잘 했다면 randomNumber에 만들어온 난수가 잘 담겨있을 것이기 때문에
+                        // if문으로 빈 문자열인지 확인하고 아니라면 난수의 길이를 랜덤 숫자의 자릿수를 저장해두는
+                        // randomDigits에 set.
+                        randomNumber = game.createChoiceRandomNumber(userChoiceDigits);
                         if(randomNumber != ""){
                             game.setRandomDigits(randomNumber.length());
                         }
                         System.out.println("< 게임을 시작합니다 >");
-                        System.out.println("중복 숫자 없는 "+userChoiceNumber+" 자릿수 맞추기 ! ");
+                        System.out.println("중복 숫자 없는 "+userChoiceDigits+" 자릿수 맞추기 ! ");
                         break;
                     case "1":
                         System.out.println("< 게임을 시작합니다 >");
                         System.out.println("중복 숫자 없는 3자릿수 맞추기 !");
                         break;
                     case "2":
-                        int num = 1;
+                        int resultNumber = 1;
                         System.out.println("---- 게임 기록 보기 ! -----");
                         ArrayList<Integer> result = new ArrayList<>();
                         ArrayList<Integer> cnt = new ArrayList<>();
+                        // 게임 결과를 저장해둔 result 리스트를 가져옴
                         result = game.getResult();
+                        // 매 게임마다 도전 횟수를 카운트해서 저장해둔 cnt 리스트를 가져옴.
                         cnt = game.getReplayCnt();
+                        // for문을 돌면서 출력해준다.
                         for(int i = 0; i < result.size(); i++){
-                            System.out.println(num +". "+result.get(i)+" / 도전 횟수 : "+cnt.get(i));
-                            num++;
+                            System.out.println(resultNumber +". "+result.get(i)+" / 도전 횟수 : "+cnt.get(i));
+                            resultNumber++;
                         }
                         continue;
                     case "3":
@@ -52,13 +70,17 @@ public class GameStart {
                         System.out.println("< 숫자 야구 게임을 종료합니다 >");
                         System.out.println("또 이용해주세요 !");
                         return true;
+                        // 0 ~ 4 이외에 것들을 입력하면 예외처리
                     default: throw new BadInputException("숫자를 0 ~ 4까지 다시 입력해주세요");
                 }
+                // 테스트하기위해 만들어진 랜덤 숫자가 뭔지 sout으로 출력
                 System.out.println(randomNumber);
                 num++;
             }
             System.out.print("숫자를 입력하세요 : ");
             String userInput = sc.nextLine();
+            // 정답을 맞췄을 때만 true를 리턴 하게 해뒀기 때문에
+            // 이 if문 안에 들어간다면 정답을 맞췄을 때 뿐이다.
             if (game.filterNum(randomNumber,userInput)) {
                 System.out.println("정답 입니다.");
                 replayCnt++;
@@ -67,6 +89,9 @@ public class GameStart {
                 num = 0;
                 break;
             }
+            // 정답을 못 맞췄다면
+            // 스트라이크 갯수와 볼 갯수를 가져와서
+            // 출력문으로 출력
             int strike = game.getStrike();
             int ball = game.getBall();
             if (strike > 0 && ball > 0) {
@@ -78,6 +103,7 @@ public class GameStart {
             } else if (strike == 0 && ball > 0) {
                 System.out.println(ball + " 볼");
                 replayCnt++;
+                // 아무것도 맞추지 못했다면 아웃을 출력
             } else {
                 System.out.println("아웃");
                 replayCnt++;
